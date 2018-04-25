4f12f865e1aebfd2496e916d83727a84030b7cf3
nacim.issam <nacim.issam@gmail.com>
2018-04-25 23:37:17 +0100

    first commit

---
+++ src/main/java/com/example/howtodoinjava/elkexamplespringboot/ELKResources.java
@@ -0,0 +1,22 @@
+package com.example.howtodoinjava.elkexamplespringboot;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by issam on 4/25/18.
+ */
+@RestController
+class ELKResources {
+    private static final Logger LOG = Logger.getLogger(ELKResources.class.getName());
+
+    @RequestMapping(value = "/ElkWelcome")
+    public String helloWorld() {
+        String welcome = "Welcome to ELK with Spring Boot by Nacim Issam";
+        LOG.log(Level.INFO, welcome);
+
+        return welcome;
+    }
+}
---
+++ src/main/java/com/example/howtodoinjava/elkexamplespringboot/ElkExampleSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.howtodoinjava.elkexamplespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkExampleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkExampleSpringBootApplication.class, args);
+	}
+}
+
+
